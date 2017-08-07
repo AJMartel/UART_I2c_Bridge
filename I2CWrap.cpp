6aa94418933f1e2314378a34a8b5c2a0b0eea88b
Mihai Galos <mihaigalos@yahoo.com>
2017-08-07 22:23:45 +0200

    Add logic to the I2CWrapper

--- I2CWrap.cpp
+++ I2CWrap.cpp
@@ -1,12 +1,21 @@
 #include "I2CWrap.h"
 #include <Wire.h>
 
+#if ARDUINO >= 100
+#define WireTransfer Wire.write
+#else
+#define WireTransfer Wire.send
+#endif
 void I2CWrap::run(const int &destination, const std::vector<String> &sendBytes,
                   const int &expectedByteCount) {
-  Wire.begin();
-  Wire.requestFrom(destination,
-                   expectedByteCount); // request 6 bytes from slave device #8
-  while (Wire.available()) {
-    response_.push_back(static_cast<uint8_t>(Wire.read()));
+  Wire.beginTransmission(destination);
+
+  auto remoteRegister = sendBytes[0].toInt();
+  WireTransfer(remoteRegister);
+
+  for (const auto &it = sendBytes.rbegin(); it != sendBytes.rend() - 1; ++it) {
+    WireTransfer((*it).toInt());
   }
+
+  Wire.endTransmission();
 }
