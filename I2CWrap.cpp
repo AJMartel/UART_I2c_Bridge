c0c6948884922ba05897b4ad7bc0cf3a95507c95
Mihai Galos <mihaigalos@yahoo.com>
2017-08-07 22:43:29 +0200

    Add parsing of hex in I2CWrap

--- I2CWrap.cpp
+++ I2CWrap.cpp
@@ -6,15 +6,56 @@
 #else
 #define WireTransfer Wire.send
 #endif
+
+// adapted from
+// https://github.com/benrugg/Arduino-Hex-Decimal-Conversion/blob/master/hex_dec.ino
+unsigned int hexToDec(const String &hexString) {
+
+  unsigned int decValue = 0;
+  int nextInt;
+
+  int i = 0;
+  if ('x' == hexString[1])
+    i = 2; // skip;
+
+  for (; i < hexString.length(); i++) {
+
+    nextInt = int(hexString.charAt(i));
+    if (nextInt >= 48 && nextInt <= 57)
+      nextInt = map(nextInt, 48, 57, 0, 9);
+    if (nextInt >= 65 && nextInt <= 70)
+      nextInt = map(nextInt, 65, 70, 10, 15);
+    if (nextInt >= 97 && nextInt <= 102)
+      nextInt = map(nextInt, 97, 102, 10, 15);
+    nextInt = constrain(nextInt, 0, 15);
+
+    decValue = (decValue * 16) + nextInt;
+  }
+
+  return decValue;
+}
+
 void I2CWrap::run(const int &destination, const std::vector<String> &sendBytes,
                   const int &expectedByteCount) {
+
+  Serial.print("Destination : ");
+  Serial.println(destination);
+
   Wire.beginTransmission(destination);
 
-  auto remoteRegister = sendBytes[0].toInt();
-  WireTransfer(remoteRegister);
+  auto remoteRegister = hexToDec(sendBytes[0]);
+  // WireTransfer(remoteRegister);
+  Serial.print("RemoteRegister : ");
+  Serial.println(remoteRegister);
+
+  if (sendBytes.rbegin() != sendBytes.rend() - 1) {
+    Serial.println();
+    Serial.println("Argument bytes : ");
+  }
 
   for (const auto &it = sendBytes.rbegin(); it != sendBytes.rend() - 1; ++it) {
-    WireTransfer((*it).toInt());
+    // WireTransfer(hexToDec(*it));
+    Serial.println(hexToDec(*it));
   }
 
   Wire.endTransmission();
