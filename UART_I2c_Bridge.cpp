647cb10e36f055457dae05133cbe6012d00a005a
Mihai Galos <mihaigalos@yahoo.com>
2017-08-07 21:36:05 +0200

    Initial Commit: Parse of UART input

---
+++ UART_I2c_Bridge.cpp
@@ -0,0 +1,52 @@
+
+
+#include "Arduino.h"
+#include "UART_I2c_Bridge.h"
+
+static constexpr uint8_t kHexLength = 4; // format 0xAB
+
+void Uart2I2CBridge::print(const String& originalString){
+  Serial.println("Got : "+originalString);
+  Serial.print(destination_+"   "+expectedByteCount_+"   ");
+  for(const auto& sendByte : sendBytes_){
+    Serial.print( sendByte +"   ");
+  }
+  
+  Serial.println(expectedByteCount_);
+}
+
+void Uart2I2CBridge::readSendBytes(const String &in, long int  sendByteCount, uint8_t& newPos, uint8_t& commaOffset){
+  
+  ;
+  auto posSendByteBegin = kHexLength+3;
+  auto i = 0;
+  commaOffset = 0;
+  for(; i < sendByteCount; ++i){
+    
+    if (i > 0) commaOffset = 1;
+    String sendByte = in.substring(posSendByteBegin+i*kHexLength+commaOffset,posSendByteBegin+kHexLength+i*kHexLength+commaOffset);  
+    sendBytes_.push_back(sendByte);
+  }
+  newPos = posSendByteBegin+i*kHexLength+commaOffset+1;
+}
+
+void Uart2I2CBridge::readFormattedString(bool isPrint=true){
+// Format: 0x<HEX Address of Destination>:<Number of send Bytes>:0x<Send Byte0>[,0x<Send Byte1>[,...]]:<Number of expected bytes>
+// Example1 : 0x40:1:0x12:1
+// Example2 : 0x40:2:0x12,0xEF:1
+
+  while (Serial.available()==0);
+  String str =Serial.readString(); 
+  
+  destination_= str.substring(0,kHexLength);
+  sendByteCount_= str.substring(kHexLength+1,kHexLength+2);
+  
+  uint8_t posExpectedByteCount = 0, commaOffset = 0;;
+  readSendBytes(str, sendByteCount_.toInt(), posExpectedByteCount, commaOffset);
+  
+  expectedByteCount_ = str.substring(posExpectedByteCount,posExpectedByteCount+1);  
+  
+  if(isPrint){
+    print(str);
+  }
+}
---
+++ UART_I2c_Bridge.ino
@@ -0,0 +1,22 @@
+
+#include "UART_I2c_Bridge.h"
+
+Uart2I2CBridge bridge;
+
+
+
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  Serial.println("Setup ready.");
+}
+
+
+
+void loop() {
+  bridge.readFormattedString();
+  
+  
+}
+
